0a32d4dc64fdc2041fa8ca838c3cf3c3b338d59a
lucas silva sarmento <lucassilvasarmentos3@gmail.com>
2025-07-14 17:28:12 -0300

    Primeiros passos

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import './styles/theme.css';
+import './styles/global.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+      <h1>olá</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste tempora
+        reprehenderit fuga dignissimos officiis! Dolores, aliquam vero numquam
+        dolore culpa debitis et facere, libero tempore similique repellendus.
+        Ducimus, debitis accusamus.
       </p>
     </>
   );
 }
-
-export default App;
